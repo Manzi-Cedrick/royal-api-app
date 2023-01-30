4081646254c4d76452e6248396885c242a5d6dd3
Manzi Cedrick <cedrickmanzii0@gmail.com>
2023-01-30 23:56:15 +0200

    [add] token --login functionality -up

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -7,11 +7,21 @@
                         <h2 class="text-2xl font-bold uppercase mb-1">
                             Log In
                         </h2>
-                        <p class="mb-4">Log in to post gigs</p>
+                        <p class="mb-4">Welcome to our application. Feel free to explore from the recent view.</p>
                     </header>
-
-                    <form action="/login/authenticate" method="POST">
+                    @if (Session::has('success'))
+                    <div class="flex p-4 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 drk:bg-gray-800 dark:text-green-400 dark:border-green-800" role="alert">
+                        <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+                        <span class="sr-only">Info</span>
+                    <div>
+                        <span class="font-medium"> {{ Session::get('success') }}!</span> Change a few things up and try submitting again.
+                    </div>
+                    </div>
+                    @endif
+                    <form action="/authenticate" method="POST">
                         @csrf
+                        @method('POST')
+                        
                         <div class="mb-6">
                             <label for="email" class="inline-block text-lg mb-2"
                                 >Email</label
