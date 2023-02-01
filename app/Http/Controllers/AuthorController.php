7aaf4d7a0191f2c3ddba15d664af23de3521387b
Manzi Cedrick <cedrickmanzii0@gmail.com>
2023-02-01 15:10:11 +0200

    [update] formating and fixes handling

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,17 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Author;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use GuzzleHttp\Client;
 class AuthorController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $token = Session::get('token_key');
@@ -33,39 +26,10 @@ class AuthorController extends Controller
 
         $data = json_decode($response->getBody(), true);
         $items= $data['items'];
-        // dd($data);
         return view('authors.index', [
             'data' => $items,
         ]);
-        // return view('authors.index');
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Author  $author
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
@@ -81,50 +45,14 @@ class AuthorController extends Controller
                 'direction' => 'ASC',
                 'limit' => 12,
                 'page' => 1,
-                // 'id' => $request
             ],
         ]);
         $data = json_decode($response->getBody(), true);
         $books = $data['books'];
         $first_name = $data['first_name'];
-   
         return view('authors.books', [
             'title' => $first_name,
             'data' => $books
         ]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Author  $author
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Author $author)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Author  $author
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Author $author)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Author  $author
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Author $author)
-    {
-        //
     }
 }
